2332fece75b5362ac82fbeb5d263be18aa29826d
Pax-Newman <newmanp@wwu.edu>
2023-01-19 23:17:24 -0800

    added a main function to test the lexer with

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
-package kaleidoscope
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	in := `def helloworld
+	return 10`
+
+	fmt.Println("Creating Lexer")
+	lex := NewLexer(
+		bytes.NewReader(
+			[]byte(in),
+		),
+	)
+
+	fmt.Println("Getting Channel")
+	tokens := lex.Tokens()
+
+	fmt.Println("Running Lexer")
+	go lex.Run()
+
+	for tok := range tokens {
+		fmt.Printf("Got Token: %T%s\n", tok, tok)
+	}
+
+}
